cbbf45f56fc43315ee40a204b8cac96a6e551cb9
fabrizio.debortoli@microarea.it <fabrizio.debortoli@microarea.it>
2017-10-18 09:30:14 +0200

    1.0.0-beta.0

--- taskbuilder-icons.umd.js
+++ taskbuilder-icons.umd.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@angular/core'), require('@angular/common')) :
 	typeof define === 'function' && define.amd ? define(['exports', '@angular/core', '@angular/common'], factory) :
-	(factory((global['taskbuilder-icons'] = {}),global._angular_core,global._angular_common));
-}(this, (function (exports,_angular_core,_angular_common) { 'use strict';
+	(factory((global['taskbuilder-icons'] = {}),global.core,global.common));
+}(this, (function (exports,core,common) { 'use strict';
 
 var M4IconComponent = (function () {
     function M4IconComponent() {
@@ -11,7 +11,7 @@ var M4IconComponent = (function () {
     return M4IconComponent;
 }());
 M4IconComponent.decorators = [
-    { type: _angular_core.Component, args: [{
+    { type: core.Component, args: [{
                 selector: 'm4-icon',
                 styles: [""],
                 template: "<i class=\"m4-icon m4-{{icon}}\"></i>"
@@ -22,7 +22,7 @@ M4IconComponent.decorators = [
  */
 M4IconComponent.ctorParameters = function () { return []; };
 M4IconComponent.propDecorators = {
-    'icon': [{ type: _angular_core.Input, args: ['icon',] },],
+    'icon': [{ type: core.Input, args: ['icon',] },],
 };
 
 var TbIconsModule = (function () {
@@ -31,8 +31,8 @@ var TbIconsModule = (function () {
     return TbIconsModule;
 }());
 TbIconsModule.decorators = [
-    { type: _angular_core.NgModule, args: [{
-                imports: [_angular_common.CommonModule],
+    { type: core.NgModule, args: [{
+                imports: [common.CommonModule],
                 declarations: [M4IconComponent],
                 exports: [M4IconComponent]
             },] },
